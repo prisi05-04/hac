866d5f1e405f41ffb9fcf163e57e15d8fcb0d708
prisi05-04 <prisi.santhan_bit27@mepcoeng.ac.in>
2026-02-04 10:28:48 +0530

    Update interface.java

--- interface.java
+++ interface.java
@@ -1,19 +1,21 @@
 import java.util.*;
+
 interface AdvancedArithmetic{
-  int divisor_sum(int n);
+    int divisorSum(int n);
 }
+
 class MyCalculator implements AdvancedArithmetic{
-   public int divisor_sum(int n){
-     int sum=0;
-        for(int i=0;i<n;i++){
-            if (n % i == 0) {
-             sum += i;
+    public int divisorSum(int n){
+        int sum = 0;
+    
+        for (int i = 1; i <= n; i++) {
+            if (n % i == 0) { 
+                sum += i;
+            }
         }
+        return sum;
     }
-    return sum;
 }
-}
-//Write your code here
 
 class Solution{
     public static void main(String []args){
@@ -22,12 +24,10 @@ class Solution{
         ImplementedInterfaceNames(my_calculator);
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        System.out.print(my_calculator.divisor_sum(n) + "\n");
-      	sc.close();
+        System.out.print(my_calculator.divisorSum(n) + "\n");
+        sc.close();
     }
-    /*
-     *  ImplementedInterfaceNames method takes an object and prints the name of the interfaces it implemented
-     */
+
     static void ImplementedInterfaceNames(Object o){
         Class[] theInterfaces = o.getClass().getInterfaces();
         for (int i = 0; i < theInterfaces.length; i++){
@@ -36,4 +36,3 @@ class Solution{
         }
     }
 }
-
