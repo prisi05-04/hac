4cbce5c6a79e1c72826a9d0d398d3bc54b79aa63
prisi05-04 <prisi.santhan_bit27@mepcoeng.ac.in>
2026-02-04 10:27:34 +0530

    Update inherit2.java

--- inherit2.java
+++ inherit2.java
@@ -2,29 +2,22 @@ import java.io.*;
 import java.util.*;
 import java.text.*;
 import java.math.*;
-import java.util.regex.*;
-
-//Write your code here
-class Arithmetic{
-    public int add(int a,int b){
-      return a+b;
-}
-}
-class Adder extends Arithmetic{
-    
+import java.util.regex.
+class Arithmetic {
+    public int add(int a, int b) {
+        return a + b;
+    }
 }
 
+class Adder extends Arithmetic {
+}
 
-
-public class Solution{
-    public static void main(String []args){
-        // Create a new Adder object
+public class Solution {
+    public static void main(String[] args) {
         Adder a = new Adder();
         
-        // Print the name of the superclass on a new line
         System.out.println("My superclass is: " + a.getClass().getSuperclass().getName());	
         
-        // Print the result of 3 calls to Adder's `add(int,int)` method as 3 space-separated integers:
-        System.out.print(a.add(10,32) + " " + a.add(10,3) + " " + a.add(10,10) + "\n");
-     }
+        System.out.print(a.add(10, 32) + " " + a.add(10, 3) + " " + a.add(10, 10) + "\n");
+    }
 }
